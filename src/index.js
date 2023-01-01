efc67b424e873e0575238a3baa39c93e703b0d75
Isaac Yanez <isaac231467@gmail.com>
2023-01-01 17:18:33 -0600

    forgot to add the the .env file and the .gitignore before :()

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 // Creating a variable that is holding an instance of the express server
 const app = express();
 
+// This will allow us to parse the JSON data that we will be receiving
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Here we are saving a varaiable with the PORT that we are going to listen on
 const PORT = 3000;
 
