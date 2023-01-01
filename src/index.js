5d4473867153e5aa3a1bd49833addd806e5a572a
Isaac Yanez <isaac231467@gmail.com>
2023-01-01 17:54:11 -0600

    was able to successfully connect to the db

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Creating a variable that is holding an instance of the express server
 const app = express();
@@ -9,6 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // Here we are saving a varaiable with the PORT that we are going to listen on
 const PORT = 3000;
+const mongooseUri = process.env.MONGO_URI;
+
+mongoose.connect(mongooseUri);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error connected to mongo", err);
+});
 
 // This will allow us to receive a notification in our terminal that our server is on and listening
 app.listen(PORT, () => {
