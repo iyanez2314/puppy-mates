de4d967a36a520cd581b0534295320661b3cbd92
Isaac Yanez <isaac231467@gmail.com>
2023-01-07 23:41:25 -0600

    refactored the users PUT method

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,18 +5,17 @@ const User = mongoose.model("User");
 const router = express.Router();
 
 // TODO: NEED TO CHANGE THIS UP TO MATCH MY API SET UP
-router.put("/user/:id", (req, res) => {
-  User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-    .then((userData) => {
-      if (!userData) {
-        res.status(404).json({ message: "No user found with that Id!" });
-        return;
-      }
-      res.json(userData);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+router.put("/user/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    res.send(updatedUser);
+  } catch (error) {
+    return res.sendStatus(400).send(error);
+  }
 });
 
 // Fetch All users
@@ -25,7 +24,7 @@ router.get("/users", async (req, res) => {
     const getAllUsers = await User.find().populate("posts");
     res.send(getAllUsers);
   } catch (error) {
-    res.status(400).json(error);
+    return res.sendStatus(400).json(error);
   }
 });
 
