4a6c95a8e2730f1bfe67623f2950a59b419eeb91
Isaac Yanez <isaac231467@gmail.com>
2023-01-12 13:41:31 -0600

    forgot to save the file before I made the push

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./UserRoutes");
+const dogRoutes = require("./dogRoutes");
 
 router.use("/users", userRoutes);
+router.use("/users/", dogRoutes);
 
 module.exports = router;
