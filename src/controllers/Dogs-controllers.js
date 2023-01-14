0a42d60762df25f6ecbbfacf53a867930a2d0131
Isaac Yanez <isaac231467@gmail.com>
2023-01-14 10:10:23 -0600

    added another controller that will allow me to get all the dogs that are assosciated with the user

--- src/controllers/Dogs-controllers.js
+++ src/controllers/Dogs-controllers.js
@@ -12,6 +12,22 @@ const dogControllers = {
         res.status();
       });
   },
+  //GET all the dogs associated with Owner => GET /api/dogs/:ownerId
+  getAllDogsAssociatedWithOwner({ params }, res) {
+    const userId = params.userId;
+    User.findOne({ _id: userId })
+      .populate("dogs")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this Id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json({ message: "Sorry this user does not exsist!" });
+      });
+  },
+
   addDogToUser({ params, body }, res) {
     const userId = params.userId;
     const newDog = body;
--- src/routes/api/dogRoutes.js
+++ src/routes/api/dogRoutes.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const {
   getAllDogs,
   addDogToUser,
+  getAllDogsAssociatedWithOwner,
 } = require("../../controllers/Dogs-controllers");
 
 router.route("/").get(getAllDogs);
+
+router.route("/:userId").get(getAllDogsAssociatedWithOwner);
 
 router.route("/:userId/dogs").post(addDogToUser);
 
